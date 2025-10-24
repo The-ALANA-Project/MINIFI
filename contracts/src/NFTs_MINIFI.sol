f40e99cc338d571747337b01380e54023bd56081
ManuelElias <elias.soria.juan.manuel@gmail.com>
2025-10-24 15:24:45 -0400

    NFTs contract deployed and verified in Base Mainnet

--- contracts/src/NFTs_MINIFI.sol
+++ contracts/src/NFTs_MINIFI.sol
@@ -5,75 +5,113 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MINIFI_NFT is ERC721URIStorage, Ownable {
-    // Counter for the next available token ID
-    uint256 public nextTokenId = 1;
-    // Address of the USDC token contract
-    address public usdc = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
-    // Address of the Unlock Protocol (UP) contract
-    address public up = 0xaC27fa800955849d6D17cC8952Ba9dD6EAA66187;
-    // Address designated to receive USDC payments
-    address public treasury = 0xd806A01E295386ef7a7Cea0B9DA037B242622743;
+/* Smart Contract deployed in Base Mainnet
+ * Link: https://basescan.org/address/0x6D271dc007c8e96f9dFD15aa71bd55c00314a9C4#code
+ */
 
-    // Mapping to track if an address has already minted a free NFT
-    mapping(address => bool) public hasMintedFree;
+contract MINIFI_NFT is ERC721URIStorage {
+    struct NFTData {
+        string name;
+        string uri;
+    }
 
-    // Constructor initializes the ERC721 contract with a name and symbol,
-    // and sets the deployer as the owner.
-    constructor() ERC721("MINIFI", "MIFI") Ownable(msg.sender) {}
+    NFTData[] public nftTypes; // NFT types available
+    IERC20 public usdc;
+    address public owner;
+    uint256 public randomPrice; // 1 USDC
+    uint256 public selectPrice; // 3 USDC
+    uint256 private _tokenCounter;
 
-    /**
-     * @dev Allows a user to mint one free NFT.
-     * Requires that the sender has not previously minted a free NFT.
-     * @param tokenURI_ The URI for the NFT's metadata.
-     */
-    function mintFreeRandom(string memory tokenURI_) external {
-        require(!hasMintedFree[msg.sender], "You have already minted the free NFT");
-        _mintToken(msg.sender, tokenURI_);
+    mapping(uint256 => uint256) public tokenIdToType; // tokenId => nftTypes index
+    mapping(address => bool) public hasMintedFree; // free mint per wallet control
+
+    constructor(address _usdc) ERC721("UniqueNFT", "UNFT") {
+        owner = msg.sender;
+        usdc = IERC20(_usdc);
+        randomPrice = 1e6; // 1 USDC (6 decimals)
+        selectPrice = 3e6; // 3 USDC
+
+        // Add 10 NFTs with name and link
+        nftTypes.push(NFTData("Sunnyo", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeib43fgjgnrzq52cqqb67l2d7zbpdpnunyzhljjpqoi4p4ecdsllxy"));
+        nftTypes.push(NFTData("Jellodras", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeihdd4epuqqh5ai3kid3q5rl4cse7fs6jzvlg6xiwelkxf5jz7ehuq"));
+        nftTypes.push(NFTData("Lumibel", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeiesypnxl65maah2aq3bud3y7qfmykmxshxdstzm64huzsfownm7h4"));
+        nftTypes.push(NFTData("Snoodella", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeih5gjgkeooxc4y4mpjl7gggyobjuxua2h5lqd7jmkyau6266n34ti"));
+        nftTypes.push(NFTData("Melloba", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeibm2cv3gc3inh4swm3cbsjin3lmuc6ddwqqsl546gqv2jrd6o4u3m"));
+        nftTypes.push(NFTData("Wistrow", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeih2it3ta2lrdugjp4isr7krymat57yvsan53gnqhyno3kx3vqzrue"));
+        nftTypes.push(NFTData("Dozuki", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeidogcawjm56tx6dteh4k7jqrqtdgalzncjifri5ddqykeheza6aoe"));
+        nftTypes.push(NFTData("Orbitron", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeifytibngk2ovr74obdhxaxwmx6lfugxiav5y6ahwjh4xl5a5v5nby"));
+        nftTypes.push(NFTData("Solara", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeihd4kfmvnocdbryog3vasuu2wi6cjzk6vqx6hwmamxhtu3u3npyvm"));
+        nftTypes.push(NFTData("Bubbit", "https://teal-united-parrot-418.mypinata.cloud/ipfs/bafybeiaqamoffhahdihwhih2wxfzn4ieegbtrgskdhtwr7ezrh4lcpx5ve"));
+    }
+
+    // -----------------------------
+    // Free random mint (only once per wallet)
+    // -----------------------------
+    function mintRandomFree() external {
+        require(!hasMintedFree[msg.sender], "Free mint already used");
+
+        uint256 index = _getRandomIndex(msg.sender);
+        uint256 tokenId = _tokenCounter;
+        _tokenCounter++;
+
+        tokenIdToType[tokenId] = index;
         hasMintedFree[msg.sender] = true;
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, nftTypes[index].uri);
     }
 
-    /**
-     * @dev Allows a user to mint a random NFT by paying 1 USDC.
-     * Transfers 1 USDC from the sender to the treasury address.
-     * @param tokenURI_ The URI for the NFT's metadata.
-     */
-    function mintRandom(string memory tokenURI_) external {
-        uint256 amount = 1 * 10**6; // USDC has 6 decimals
-        IERC20(usdc).transferFrom(msg.sender, treasury, amount);
-        _mintToken(msg.sender, tokenURI_);
+    // -----------------------------
+    // Paid random mint 1 USDC
+    // -----------------------------
+    function mintRandomPaid() external {
+        require(usdc.transferFrom(msg.sender, owner, randomPrice), "USDC payment failed");
+
+        uint256 index = _getRandomIndex(msg.sender);
+        uint256 tokenId = _tokenCounter;
+        _tokenCounter++;
+
+        tokenIdToType[tokenId] = index;
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, nftTypes[index].uri);
     }
 
-    /**
-     * @dev Allows a user to mint a selected NFT by paying 3 USDC.
-     * Transfers 3 USDC from the sender to the treasury address.
-     * @param tokenURI_ The URI for the NFT's metadata.
-     */
-    function mintSelected(string memory tokenURI_) external {
-        uint256 amount = 3 * 10**6; // USDC has 6 decimals
-        IERC20(usdc).transferFrom(msg.sender, treasury, amount);
-        _mintToken(msg.sender, tokenURI_);
+    // -----------------------------
+    // Paid select mint 3 USDC
+    // -----------------------------
+    function mintSelect(uint256 index) external {
+        require(index < nftTypes.length, "Invalid index");
+        require(usdc.transferFrom(msg.sender, owner, selectPrice), "USDC payment failed");
+
+        uint256 tokenId = _tokenCounter;
+        _tokenCounter++;
+
+        tokenIdToType[tokenId] = index;
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, nftTypes[index].uri);
     }
 
-    /**
-     * @dev Internal helper function to mint an NFT and set its URI.
-     * Increments the nextTokenId for subsequent mints.
-     * @param to The address to which the NFT will be minted.
-     * @param tokenURI_ The URI for the NFT's metadata.
-     */
-    function _mintToken(address to, string memory tokenURI_) internal {
-        uint256 tokenId = nextTokenId;
-        nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI_);
+    // -----------------------------
+    // Get NFT name by tokenId
+    // -----------------------------
+    function getTokenName(uint256 tokenId) external view returns (string memory) {
+        uint256 index = tokenIdToType[tokenId];
+        return nftTypes[index].name;
     }
 
-    /**
-     * @dev Allows the contract owner to update the treasury address.
-     * This is an optional function to change where USDC payments are sent.
-     * @param newTreasury The new address for the treasury.
-     */
-    function setTreasury(address newTreasury) external onlyOwner {
-        treasury = newTreasury;
+    // -----------------------------
+    // Internal function to get random index
+    // -----------------------------
+    function _getRandomIndex(address user) internal view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.timestamp, user, blockhash(block.number - 1)))) % nftTypes.length;
+    }
+
+    // -----------------------------
+    // Block burn
+    // -----------------------------
+    function burn(uint256) external pure {
+        revert("Burn not allowed");
     }
 }
